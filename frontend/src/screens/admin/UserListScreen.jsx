c2d0f9419317b2720cb374daed3d1a84f29a66c9
Sujit Mohanty <sujit.mty@outlook.com>
2023-05-04 19:03:30 +0530

    fix 401 unauthorised error courtesy of vite config

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Table, Button } from 'react-bootstrap';
-import { FaTrash, FaEdit, FaCheck, FaTimes } from 'react-icons/fa';
-import Message from '../../components/Message';
-import Loader from '../../components/Loader';
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
 import {
   useDeleteUserMutation,
   useGetUsersQuery,
-} from '../../slices/usersApiSlice';
-import { toast } from 'react-toastify';
+} from "../../slices/usersApiSlice";
+import { toast } from "react-toastify";
 
 const UserListScreen = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
@@ -16,7 +15,7 @@ const UserListScreen = () => {
   const [deleteUser] = useDeleteUserMutation();
 
   const deleteHandler = async (id) => {
-    if (window.confirm('Are you sure')) {
+    if (window.confirm("Are you sure")) {
       try {
         await deleteUser(id);
         refetch();
@@ -32,18 +31,17 @@ const UserListScreen = () => {
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>
+        <Message variant="danger">
           {error?.data?.message || error.error}
         </Message>
       ) : (
-        <Table striped bordered hover responsive className='table-sm'>
+        <Table striped bordered hover responsive className="table-sm">
           <thead>
             <tr>
               <th>ID</th>
               <th>NAME</th>
               <th>EMAIL</th>
               <th>ADMIN</th>
-              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -56,32 +54,30 @@ const UserListScreen = () => {
                 </td>
                 <td>
                   {user.isAdmin ? (
-                    <FaCheck style={{ color: 'green' }} />
+                    <FaCheck style={{ color: "green" }} />
                   ) : (
-                    <FaTimes style={{ color: 'red' }} />
+                    <FaTimes style={{ color: "red" }} />
                   )}
                 </td>
-                <td>
-                  {!user.isAdmin && (
-                    <>
-                      <LinkContainer
-                        to={`/admin/user/${user._id}/edit`}
-                        style={{ marginRight: '10px' }}
-                      >
-                        <Button variant='light' className='btn-sm'>
-                          <FaEdit />
-                        </Button>
-                      </LinkContainer>
-                      <Button
-                        variant='danger'
-                        className='btn-sm'
-                        onClick={() => deleteHandler(user._id)}
-                      >
-                        <FaTrash style={{ color: 'white' }} />
+                {!user.isAdmin && (
+                  <td>
+                    <LinkContainer
+                      to={`/admin/user/${user._id}/edit`}
+                      style={{ marginRight: "10px" }}
+                    >
+                      <Button variant="light" className="btn-sm">
+                        <FaEdit />
                       </Button>
-                    </>
-                  )}
-                </td>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(user._id)}
+                    >
+                      <FaTrash style={{ color: "white" }} />
+                    </Button>
+                  </td>
+                )}
               </tr>
             ))}
           </tbody>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      api: "http://localhost:8585",
+      "/api": {
+        target: "http://localhost:8585",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   plugins: [react()],
