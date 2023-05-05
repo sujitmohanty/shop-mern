ce2266e6268ce0f2fa2f670f23eb2a19024e751b
Sujit Mohanty <sujit.mty@outlook.com>
2023-05-05 14:08:22 +0530

    add user crud feature for other admin users

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button } from "react-bootstrap";
 import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
@@ -42,6 +43,7 @@ const UserListScreen = () => {
               <th>NAME</th>
               <th>EMAIL</th>
               <th>ADMIN</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -59,25 +61,27 @@ const UserListScreen = () => {
                     <FaTimes style={{ color: "red" }} />
                   )}
                 </td>
-                {!user.isAdmin && (
-                  <td>
-                    <LinkContainer
-                      to={`/admin/user/${user._id}/edit`}
-                      style={{ marginRight: "10px" }}
-                    >
-                      <Button variant="light" className="btn-sm">
-                        <FaEdit />
+                <td>
+                  {user && (
+                    <>
+                      <LinkContainer
+                        to={`/admin/userlist/${user._id}/edit`}
+                        style={{ marginRight: "10px" }}
+                      >
+                        <Button variant="light" className="btn-sm">
+                          <FaEdit />
+                        </Button>
+                      </LinkContainer>
+                      <Button
+                        variant="danger"
+                        className="btn-sm"
+                        onClick={() => deleteHandler(user._id)}
+                      >
+                        <FaTrash style={{ color: "white" }} />
                       </Button>
-                    </LinkContainer>
-                    <Button
-                      variant="danger"
-                      className="btn-sm"
-                      onClick={() => deleteHandler(user._id)}
-                    >
-                      <FaTrash style={{ color: "white" }} />
-                    </Button>
-                  </td>
-                )}
+                    </>
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
