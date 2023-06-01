c807554a1c8029e30382480a62a30c0fcfdd8d1e
Sujit Mohanty <sujit.mty@outlook.com>
2023-06-02 01:55:23 +0530

    update proxy for uploads in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+      "/uploads": {
+        target: "http://localhost:8585",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   plugins: [react()],
